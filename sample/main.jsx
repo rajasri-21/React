ff1c7087b0a51b8831fccef5f12a201b158a39f6
rajasri-21 <166021380+rajasri-21@users.noreply.github.com>
2024-09-17 08:57:31 +0530

    Add files via upload

---
+++ sample/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+//import {app} from './app'
+import './assets/css/style.css'
+import App from './App'
+
+
+createRoot(document.getElementById('root')).render(
+  <App/>
+ 
+)
